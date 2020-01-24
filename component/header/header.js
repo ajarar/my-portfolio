30d25f9db1e8b9ca2b1a23e20300ef80b56b44d4
Ahmad Jarrar <ajarar.jo@gmail.com>
2020-01-25 01:34:29 +0200

    Header implement #2

--- component/header/header.js
+++ component/header/header.js
@@ -1,10 +1,14 @@
-let navigationBtn = document.querySelector('.site-menu-btn-menu');
-let siteMenuNav =  document.querySelector('.site-menu');
-let siteMenuNavList =  siteMenuNav.querySelector('.site-menu ul');
+const navigationBtn = document.querySelector('.site-menu-btn-menu');
+const siteMenuNav =  document.querySelector('.site-menu');
+const siteMenuNavList =  siteMenuNav.querySelector('ul');
+const siteMenuNavListItem = siteMenuNavList.querySelectorAll('button'); 
 const activeNavClass = 'active-menu';
 const hiddenNavClass = 'd-none';
+const navListItemActiveClass= 'active';
 
-let navigationBtnToogle = () => {
+
+/* Open Burger Menu */
+const navigationBtnToogle = () => {
     if(siteMenuNav && siteMenuNavList) {
         siteMenuNav.classList.toggle(activeNavClass);
         siteMenuNavList.classList.toggle(hiddenNavClass);
@@ -12,4 +16,33 @@ let navigationBtnToogle = () => {
     }
 }
 
-navigationBtn.addEventListener('click',navigationBtnToogle);
+/* Add active class to nav item */
+const navChoseActiveItem = (event) => {
+    let parentElement = event.target.parentElement;
+    let activeElement = siteMenuNav.querySelector('.active');
+    let current = activeElement.querySelector('span.sr-only');
+    console.log(current);
+    if(!event.isTrusted) {
+        return ;
+    }
+    if(parentElement) {
+        parentElement.classList.add('active');
+        event.target.appendChild(current.cloneNode(true))
+    }
+    if(activeElement) {
+        activeElement.classList.remove('active');
+        current.remove();
+    }    
+}
+
+
+/* Add Event active class to nav item */
+if(siteMenuNavListItem) {
+    [...siteMenuNavListItem].map((item) => item.addEventListener('click' , navChoseActiveItem));
+}
+
+/* Add Event for open Burger Menu */ 
+
+if(navigationBtn) {
+    navigationBtn.addEventListener('click', navigationBtnToogle);
+}
